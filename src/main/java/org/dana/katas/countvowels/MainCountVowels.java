815acce13a85d21b6377992b901a046ba888e921
kngssd <danakengessova@gmail.com>
2024-07-31 11:11:12 +0100

    add files for the next kata

---
+++ src/main/java/org/dana/katas/countvowels/MainCountVowels.java
@@ -0,0 +1,4 @@
+package org.dana.katas.countvowels;
+
+public class MainCountVowels {
+}
---
+++ src/test/java/org/dana/katas/countvowels/MainCountVowelsTest.java
@@ -0,0 +1,7 @@
+package org.dana.katas.countvowels;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainCountVowelsTest {
+
+}
